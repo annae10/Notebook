34782e7e20aba95887f2defacababbda21cae4e1
Anna Ermolaeva <ermolaeva-ann@mail.ru>
2022-05-18 23:47:39 +0300

    Main activity to add fragment

--- app/src/main/java/com/ann/noteapp/ui/MainActivity.kt
+++ app/src/main/java/com/ann/noteapp/ui/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.ann.noteapp.ui
 
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.ann.noteapp.R
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : DaggerAppCompatActivity() {
+
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.container)
+
+
+        fab.setOnClickListener {
+            navController.navigate(R.id.action_listFragment_to_addFragment)
+            fab.hide()
+        }
+    }
+
+    fun showFloatingButton(){
+        fab.show()
+        fab.visibility = View.VISIBLE
     }
 }
