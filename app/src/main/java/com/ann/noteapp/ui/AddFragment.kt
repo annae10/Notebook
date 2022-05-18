a1a0d6299378019618ca08c7716b015fa1f190c5
Anna Ermolaeva <ermolaeva-ann@mail.ru>
2022-05-18 23:47:57 +0300

    Add fragment

--- app/src/main/java/com/ann/noteapp/ui/AddFragment.kt
+++ app/src/main/java/com/ann/noteapp/ui/AddFragment.kt
@@ -1,6 +1,43 @@
 package com.ann.noteapp.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import com.ann.noteapp.R
+import com.ann.noteapp.utils.ViewModelProviderFactory
 import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.fragment_add.*
+import javax.inject.Inject
 
 class AddFragment:DaggerFragment() {
+
+    lateinit var noteViewModel: NoteViewModel
+
+    @Inject
+    lateinit var viewModelProviderFactory: ViewModelProviderFactory
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_add,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpViewModel()
+
+        btnAdd.setOnClickListener {
+            Navigation.findNavController(requireActivity(),R.id.container).popBackStack()
+        }
+    }
+
+    private fun setUpViewModel() {
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory).get(NoteViewModel::class.java)
+    }
 }
