89988a1405d28adef07acecfd43f7cebbc0b0065
Anna Ermolaeva <ermolaeva-ann@mail.ru>
2022-05-19 23:38:46 +0300

    Coroutines

--- app/src/main/java/com/ann/noteapp/repository/Repository.kt
+++ app/src/main/java/com/ann/noteapp/repository/Repository.kt
@@ -1,4 +1,29 @@
 package com.ann.noteapp.repository
 
-class Repository {
+import com.ann.noteapp.model.Note
+import com.ann.noteapp.model.NoteDao
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class Repository @Inject constructor(val noteDao: NoteDao) {
+
+    fun insert(note: Note){
+        CoroutineScope(Dispatchers.IO).launch {
+            noteDao.insert(note)
+        }
+    }
+
+    fun delete(note: Note){
+        CoroutineScope(Dispatchers.IO).launch {
+            noteDao.delete(note)
+        }
+    }
+
+    fun update(note: Note){
+        CoroutineScope(Dispatchers.IO).launch {
+            noteDao.update(note)
+        }
+    }
 }
